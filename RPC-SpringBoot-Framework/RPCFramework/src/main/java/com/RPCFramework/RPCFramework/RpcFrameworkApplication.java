cd1a63fcb36a747c4886ff212230a6fcd18454ba
Zack <hzkzqxx@163.com>
2022-06-25 12:29:40 -0700

    init three spring boot projects

---
+++ RPC-SpringBoot-Framework/RPCFramework/src/main/java/com/RPCFramework/RPCFramework/RpcFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.RPCFramework.RPCFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcFrameworkApplication.class, args);
+	}
+
+}
---
+++ RPC-SpringBoot-Framework/RPCFramework/src/test/java/com/RPCFramework/RPCFramework/RpcFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RPCFramework.RPCFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
